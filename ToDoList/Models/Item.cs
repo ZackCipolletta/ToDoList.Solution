70042e056d351b1f12ed4bc6ef07ec76eb5b5e7f
Zachary <ZackCipolletta@gmail.com>
2023-03-14 22:04:18 -0700

    home updates from 3.13.23

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
   public class Item
   {
+    [Required(ErrorMessage = "This item's description can't be empty!")]
     public string Description { get; set; }
     public int ItemId { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
     public int CategoryId { get; set; }
     public Category Category { get; set; }
     public List<ItemTag> JoinEntities { get; }
